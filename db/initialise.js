374aed5869620970e7196981601f5160eff81e6c
Sam Chung <schung2@deloitte.com.au>
2020-01-08 14:52:23 +1100

    Add persitance to db

--- db/initialise.js
+++ db/initialise.js
@@ -1,9 +1,22 @@
+const VENUES = 'venues';
 const loki = require('lokijs');
 const data = require('./mgqj-necz.json');
 const db = new loki('test.db', {autosave: true});
-const venues = db.addCollection('venues');
-venues.insert(data);
+const fs = require('fs');
+const {getPub} = require('../pubs/pubs.controller');
+
+// If collection is empty do not load it, instead - create a new file
+db.loadDatabase(null, async () => {
+  console.log("db initialising");
+  venues = db.getCollection(VENUES);
+  if (venues === null){
+    console.log('no existing db');
+    venues = db.addCollection(VENUES);
+    venues.insert(data);
+    console.log("db created");
+  }
+})
 
 module.exports = {
-  venues
+  db
 }
--- venues/venues.dal.js
+++ venues/venues.dal.js
@@ -1,13 +1,21 @@
-const {venues} = require('../db/initialise');
+const VENUES = 'venues';
+const {db} = require('../db/initialise');
+
 
 /**
  * Loads a venue from our database
  * @param {*} propertyNumber 
  */
 function loadVenue(propertyNumber){
+  const venues = db.getCollection(VENUES);
   return venues.find({
     property_number: propertyNumber
   })
+}
+
+function updateVenue(venue){
+  const venues = db.getCollection(VENUES);
+  return venues.update(venue);
 }
 
 /**
@@ -15,12 +23,18 @@ function loadVenue(propertyNumber){
  * @param {Object} venue 
  */
 function storeVenue(venue){
+  const venues = db.getCollection(VENUES);
   return venues.insert(venue);
 }
 
-
+function removeVenue(venue){
+  const venues = db.getCollection(VENUES);
+  return venues.remove(venue)
+}
 
 module.exports = {
   loadVenue,
-  storeVenue
+  storeVenue,
+  removeVenue,
+  updateVenue
 }
