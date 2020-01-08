c73eb63c53714c789c155303274d35165ab7c509
Sam Chung <schung2@deloitte.com.au>
2020-01-08 12:17:20 +1100

    initialise project

---
+++ index.js
@@ -0,0 +1,18 @@
+const Koa = require('koa');
+const bodyParser = require('koa-bodyparser');
+const app = new Koa();
+const PORT = process.env.PORT || 1337;
+const routes = require('./routes');
+require('./db/initialise');
+
+app.use(bodyParser());
+
+// routes
+app.use(routes.routes());
+
+// server
+const server = app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}`);
+});
+
+module.exports = server;
